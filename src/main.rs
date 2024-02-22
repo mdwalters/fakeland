9519f22479481bbbf9b1dff3b1de19db7b65f762
Max Walters <mdwalters.pm@proton.me>
2024-02-22 16:09:17 +0000

    tr

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
 use yew::prelude::*;
 
-#[path = "components/message.rs"] mod message;
-use message::Message;
+mod components;
 
 #[function_component]
 fn App() -> Html {
     html! {
         <>
-            <Message
+            <components::Message
                 person={ "John Fakeland" }
                 message={ "Ah yes, this is the perfect place to build." }
             />
-            <Message
+            <components::Message
                 person={ "Jane Fakeland" }
                 message={ "Ok, and?" }
             />
-            <Message
+            <components::Message
                 person={ "John Fakeland" }
                 message={ "We could create a country that is different from all of the others!" }
             />
-            <Message
+            <components::Message
                 person={ "Jane Fakeland" }
                 message={ "Do you know that every founder of every country said that, and then it winds up being literally the same as all of the others." }
             />
-            <Message
+            <components::Message
                 person={ "John Fakeland" }
                 message={ "It's not going to be the same like the others, we can have a proper economy, a good following, all while just simply being different!" }
             />
-            <Message
+            <components::Message
                 person={ "Jane Fakeland" }
                 message={ "Okay, let's try it." }
             />
 
-            <h1>{ "Start! ->" }</h1>
+            <h1>{ "Start!" }</h1>
         </>
     }
 }
